33a24da98a643f41ca5da796f75b3a90db83c034
M-Jacks <malabi1jackson@gmail.com>
2024-06-16 08:03:45 +0300

    initial commit

---
+++ sever.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 4000;
+
+// PostgreSQL connection configuration
+const pool = new Pool({
+    user: process.env.DB_USER, 
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT || 5432, // default PostgreSQL port
+  });
+
+app.get('/users', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM users');
+    res.json(result.rows);
+    client.release();
+  } catch (err) {
+    console.error(err); 
+    res.status(500).json({ error: 'Database query failed' });
+  }
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM productlist');
+    res.json(result.rows);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database query failed' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
